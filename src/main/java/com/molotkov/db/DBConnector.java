4eba829726a4b292ccd529b86ec5fa35092659e3
Mikhail Molotkov <mmolotkov1@sheffield.ac.uk>
2017-12-02 01:13:34 +0000

    Fixing tests to work with Testcontainers and Travis CI

--- src/main/java/com/molotkov/db/DBConnector.java
+++ src/main/java/com/molotkov/db/DBConnector.java
@@ -4,13 +4,21 @@ import java.sql.*;
 
 public class DBConnector {
     private String dbUrl;
+    private String dbUserName;
+    private String dbUserPasswd;
     private Connection connection;
 
-    public DBConnector(String dbUrl) {
+    public DBConnector(String dbUrl, String dbUserName, String dbUserPasswd) {
         this.dbUrl = dbUrl;
         try {
             Class.forName("org.postgresql.Driver");
-            this.connection = DriverManager.getConnection(this.dbUrl);
+            if (dbUserName.isEmpty() || dbUserPasswd.isEmpty()) {
+                this.connection = DriverManager.getConnection(this.dbUrl);
+            } else {
+                this.dbUserName = dbUserName;
+                this.dbUserPasswd = dbUserPasswd;
+                this.connection = DriverManager.getConnection(this.dbUrl, this.dbUserName, this.dbUserPasswd);
+            }
         } catch (ClassNotFoundException | SQLException ex) {
             ex.printStackTrace();
         }
@@ -18,6 +26,11 @@ public class DBConnector {
 
     public void changeDBUrl(String newUrl) {
         this.dbUrl = newUrl;
+    }
+
+    public void changeUserNameAndPasswd(String userName, String userPasswd) {
+        this.dbUserName = userName;
+        this.dbUserPasswd = userPasswd;
     }
 
     public String getDbUrl() {
