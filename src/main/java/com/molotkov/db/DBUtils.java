77054f706b693dfaf8d45f904e04a05447fbcc7d
Mikhail Molotkov <mmolotkov1@sheffield.ac.uk>
2017-12-06 16:24:54 +0000

    Debugging

--- src/main/java/com/molotkov/db/DBUtils.java
+++ src/main/java/com/molotkov/db/DBUtils.java
@@ -89,6 +89,7 @@ public class DBUtils {
             selectColumnsString = Stream.of(selectColumns).collect(Collectors.joining(", "));
         }
         final String query = String.format("SELECT %s FROM %s WHERE %s", selectColumnsString, tableName, filterArgumentsString);
+        System.out.println(query);
         final ResultSet resultSet = statement.executeQuery(query);
 
         return new DBCursorHolder(resultSet, statement);
