3c0ad1bf57998377f824a298f41983cc86082e62
Mikhail Molotkov <mmolotkov1@sheffield.ac.uk>
2017-12-07 12:15:25 +0000

    Test fixes

--- src/test/java/com/molotkov/DBUtilsTest.java
+++ src/test/java/com/molotkov/DBUtilsTest.java
@@ -17,21 +17,32 @@ import static org.rnorth.visibleassertions.VisibleAssertions.assertEquals;
 
 public class DBUtilsTest {
     private HikariConfig hikariConfig;
+    private HikariDataSource dataSource;
 
     @ClassRule
     public static PostgreSQLContainer postgres = new PostgreSQLContainer();
 
     @Before
-    public void setUp() {
+    public void setUp() throws SQLException {
         hikariConfig = new HikariConfig();
         hikariConfig.setJdbcUrl(postgres.getJdbcUrl());
         hikariConfig.setUsername(postgres.getUsername());
         hikariConfig.setPassword(postgres.getPassword());
+        dataSource = new HikariDataSource(hikariConfig);
+
+        Statement statement = dataSource.getConnection().createStatement();
+
+        statement.addBatch("CREATE TABLE IF NOT EXISTS products ( product_id serial PRIMARY KEY, product_name text NOT NULL," +
+                " product_weight numeric (6,3) NOT NULL, product_price numeric (8,2) NOT NULL )");
+        statement.addBatch("CREATE TABLE IF NOT EXISTS inventory ( entry_id serial, " +
+                "product_id int4 REFERENCES products(product_id) ON DELETE RESTRICT, product_amount int4 NOT NULL )");
+
+        statement.executeBatch();
+        statement.close();
     }
 
     @Test
     public void testDBUtilsInFullButDeleteTable() throws SQLException {
-        final HikariDataSource dataSource = new HikariDataSource(hikariConfig);
         final String[] selectColumns = {
                 "id"
         };
@@ -72,19 +83,14 @@ public class DBUtilsTest {
         assertEquals("Update query succeeds", "4", resultString4);
 
     // NATURAL JOIN TEST
-        Statement statement = dataSource.getConnection().createStatement();
-        statement.addBatch("CREATE TABLE IF NOT EXISTS products ( product_id serial PRIMARY KEY, product_name text NOT NULL," +
-                " product_weight numeric (6,3) NOT NULL, product_price numeric (8,2) NOT NULL )");
-        statement.addBatch("INSERT INTO products ( product_name, product_weight, product_price ) VALUES ( 'apple', 0.150, 0.8 )");
-        statement.addBatch("INSERT INTO products ( product_name, product_weight, product_price ) VALUES ( 'chicken', 1, 2.3 )");
-
-        statement.addBatch("CREATE TABLE IF NOT EXISTS inventory ( entry_id serial, " +
-                "product_id int4 REFERENCES products(product_id) ON DELETE RESTRICT, product_amount int4 NOT NULL )");
-        statement.addBatch("INSERT INTO inventory ( product_id, product_amount ) VALUES ( 1, 3 )");
-        statement.addBatch("INSERT INTO inventory ( product_id, product_amount ) VALUES ( 2, 4 )");
-
-        statement.executeBatch();
-        statement.close();
+        DBUtils.insertSpecificIntoTable(dataSource.getConnection(),"products",
+                new String[]{"product_name", "product_weight", "product_price"}, new String[]{"'apple'", "0.150", "0.8"});
+        DBUtils.insertSpecificIntoTable(dataSource.getConnection(),"products",
+                new String[]{"product_name", "product_weight", "product_price"}, new String[]{"'chicken'", "1", "2.3"});
+        DBUtils.insertSpecificIntoTable(dataSource.getConnection(),"inventory",
+                new String[]{"product_id", "product_amount"}, new String[]{"1", "3"});
+        DBUtils.insertSpecificIntoTable(dataSource.getConnection(),"inventory",
+                new String[]{"product_id", "product_amount"}, new String[]{"2", "4"});
 
         cursor = DBUtils.naturalJoinTables(dataSource.getConnection(), "products", "inventory", new String[] {"product_id", "product_name", "product_weight", "product_price", "product_amount"},
                 new String[]{});
@@ -102,7 +108,6 @@ public class DBUtilsTest {
 
     @Test(expected = SQLException.class)
     public void testDeleteTable() throws SQLException {
-        final HikariDataSource dataSource = new HikariDataSource(hikariConfig);
 
         DBUtils.createTable(dataSource.getConnection(), "test_table", new String[]{});
         DBUtils.deleteTable(dataSource.getConnection(), "test_table");
